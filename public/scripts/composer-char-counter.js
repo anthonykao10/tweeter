b609029e55648129139e6bf550ccf0e71ac66dc6
anthonykao10 <anthonykao10@gmail.com>
2019-09-10 21:31:02 +0000

    Update counter when inputting into textarea

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(function() {
+
+  $('.new-tweet textarea').on('input', function(e) {
+    const counter = $(this).parents('.new-tweet').find('.counter');
+    // Update counter
+    counter.text(140 - $(this).val().length);
+
+    if (counter.text() < 0) {
+      counter.css('color', '#ff0000');
+    } else {
+      counter.css('color', '');
+    }
+  });
+
+
+});
