c7f6650aceb5e69998b601f91cc8d9c45dc53593
anthonykao10 <anthonykao10@gmail.com>
2019-09-11 01:38:36 +0000

    Dynamically load tweets with sample data

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,68 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(function() {
+
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const createTweetElement = function(tweet) {
+    // let $tweet = $('<article>').addClass('tweet');
+    let daysAgo = Math.round((Date.now() - tweet.created_at) / (1000 * 60 * 60 * 24));
+
+    const $tweet = `
+      <article class="tweet">
+        <header>
+          <div>
+            <img class="display-picture" src="${tweet.user.avatars}" alt="profile">
+            <span class="name">${tweet.user.name}</span>
+          </div>
+          <span class="handle">${tweet.user.handle}</span>
+        </header>
+        <p class="content">${tweet.content.text}</p>
+        <footer>
+          <hr>
+          <div class="tweet-time">${daysAgo} days ago</div>
+          <div class="tweet-social">
+            <a href="#">flag</a>
+            <a href="#">retweet</a>
+            <a href="#">like</a>
+          </div>
+        </footer>
+      </article>
+    `;
+    return $tweet;
+  };
+
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const tweetElem = createTweetElement(tweet);
+      $('#tweets-container').append(tweetElem);
+    }
+  };
+
+  renderTweets(data);
+
+});
