09ca1bb38fb18f2ef36aea652023531a174977dd
anthonykao10 <anthonykao10@gmail.com>
2019-09-11 16:22:53 +0000

    Implement loadTweets function and call inside form submission handler

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,29 +7,29 @@
 $(function() {
 
   // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png",
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ];
 
   const createTweetElement = function(tweet) {
     // let $tweet = $('<article>').addClass('tweet');
@@ -66,9 +66,7 @@ $(function() {
     }
   };
 
-  renderTweets(data);
-  
-  // Send form data to server
+  // Listener to send form data to server
   $('.new-tweet form').submit(function(e) {
     e.preventDefault();
     $.ajax('/tweets', { 
@@ -77,8 +75,20 @@ $(function() {
     })
     .then(function (data) {
       console.log('Success: ', data);
-      // $button.replaceWith(morePostsHtml);
+      loadTweets();
     });
   });
+
+  // Fetch tweets
+  const loadTweets = function() {
+    $.ajax('/tweets', {
+      method: 'GET'
+    })
+    .then(function (data) {
+      renderTweets(data);
+    });
+  };
   
+  loadTweets();
+
 });
